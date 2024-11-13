85123b8761c03616e1ff037b920e4ad09e46ac29
ostowar.p.a <ostowar.p.a@gmail.com>
2024-11-13 14:13:12 +0330

    Formic with YUP
    formic for login and signup form .....

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,6 +1,10 @@
 import styles from "../Style/Register.module.css";
 
+import { useRef } from "react";
+
 // Note: import formic:
+import { useFormik } from "formik";
+import * as Yup from "yup";
 
 // Note: import icon from assets:
 import emailicon from "../assets/icons/email.svg";
@@ -22,7 +26,58 @@ import { EffectFlip, Pagination, Navigation } from "swiper/modules";
 // NOTE import text filde from MUI:
 import { TextField } from "@mui/material";
 import Grid from "@mui/material/Grid2";
-export default function () {
+
+// اعتبارسنجی فرم ورود
+const loginValidationSchema = Yup.object({
+  username: Yup.string().required("UserName is required"),
+  password: Yup.string().required("Password is required"),
+});
+
+// اعتبارسنجی فرم ثبت‌نام
+const signupValidationSchema = Yup.object({
+  firstName: Yup.string().required("First Name is required"),
+  lastName: Yup.string().required("Last Name is required"),
+  username: Yup.string().required("UserName is required"),
+  email: Yup.string().email("Invalid email").required("Email is required"),
+  password: Yup.string()
+    .min(6, "Password must be at least 6 characters")
+    .required("Password is required"),
+  confirmPassword: Yup.string()
+    .oneOf([Yup.ref("password"), null], "Passwords must match")
+    .required("Confirm Password is required"),
+});
+
+export default function Register() {
+  const swiperRef = useRef(null);
+  // تنظیمات فرم ورود
+  const loginFormik = useFormik({
+    initialValues: { username: "", password: "" },
+    validationSchema: loginValidationSchema,
+    validateOnChange: false, // اعتبارسنجی در هنگام تغییر فیلد غیرفعال است
+    validateOnBlur: false, // اعتبارسنجی در هنگام خروج از فیلد غیرفعال است
+    onSubmit: (values) => {
+      console.log("Login values", values);
+    },
+  });
+
+  // تنظیمات فرم ثبت‌نام
+  const signupFormik = useFormik({
+    initialValues: {
+      firstName: "",
+      lastName: "",
+      username: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+    },
+    validateOnChange: false, // اعتبارسنجی در هنگام تغییر فیلد غیرفعال است
+    validateOnBlur: false, // اعتبارسنجی در هنگام خروج از فیلد غیرفعال است
+    validationSchema: signupValidationSchema,
+    onSubmit: (values) => {
+      swiperRef.current.slideTo(0);
+    },
+  });
+
   return (
     <>
       <div
@@ -35,13 +90,17 @@ export default function () {
         <img className={styles.righthand} src={rightHand} alt="" />
         <div>
           <Swiper
+            slidesPerView={1}
+            ref={swiperRef}
             effect={"flip"}
             style={{ minWidth: "386px", width: "34vw" }}
-            modules={[EffectFlip, Pagination, Navigation]}
+            modules={[EffectFlip, Navigation]}
+            allowTouchMove={false}
+            onSwiper={(swiper) => (swiperRef.current = swiper)} // تنظیم مرجع برای Swiper
             className="mySwiper"
           >
             <SwiperSlide className={styles.card}>
-              <form action="">
+              <form onSubmit={loginFormik.handleSubmit}>
                 <Grid container spacing={2} style={{ margin: "2.4rem 2rem" }}>
                   <Grid size={2} offset={5}>
                     <img
@@ -64,25 +123,64 @@ export default function () {
                   </Grid>
 
                   <Grid container size={12}>
-                    <Grid size={12}>
+                    <Grid style={{ position: "relative" }} size={12}>
                       <input
+                        name="username"
                         type="text"
-                        className={styles.searchinput}
+                        className={styles.input}
                         placeholder="UserName*"
+                        onChange={loginFormik.handleChange}
+                        value={loginFormik.values.username}
+                        style={{
+                          border:
+                            loginFormik.touched.username &&
+                            loginFormik.errors.username
+                              ? "solid red 2px"
+                              : "",
+                        }}
                       />
+                      {/* show error: */}
+                      {loginFormik.touched.username &&
+                        loginFormik.errors.username && (
+                          <p className={styles.errorText}>
+                            {loginFormik.errors.username}
+                          </p>
+                        )}
                     </Grid>
-                    <Grid size={12}>
+                    <Grid style={{ position: "relative" }} size={12}>
                       <input
+                        name="password"
                         type="password"
-                        className={styles.searchinput}
+                        className={styles.input}
                         placeholder="Password*"
+                        onChange={loginFormik.handleChange}
+                        value={loginFormik.values.password}
+                        style={{
+                          border:
+                            loginFormik.touched.password &&
+                            loginFormik.errors.password
+                              ? "solid red 2px"
+                              : "",
+                        }}
                       />
+                      {/* show error: */}
+                      {loginFormik.touched.password &&
+                        loginFormik.errors.password && (
+                          <p className={styles.errorText}>
+                            {loginFormik.errors.password}
+                          </p>
+                        )}
                     </Grid>
 
                     <Grid size={12}>
                       <p className={styles.tologin}>
                         Don't have an account?{" "}
-                        <span className={styles.loginbtn}>SignUp</span>
+                        <span
+                          onClick={() => swiperRef.current?.slideNext()}
+                          className={styles.loginbtn}
+                        >
+                          SignUp
+                        </span>
                       </p>
                     </Grid>
                     <Grid size={12} style={{ height: "4.8rem" }}></Grid>
@@ -112,7 +210,7 @@ export default function () {
               </form>
             </SwiperSlide>
             <SwiperSlide className={styles.card}>
-              <form action="">
+              <form onSubmit={signupFormik.handleSubmit}>
                 <Grid container spacing={2} style={{ margin: "2.4rem 2rem" }}>
                   <Grid size={2} offset={5}>
                     <img
@@ -134,50 +232,172 @@ export default function () {
                   <Grid container size={12}>
                     <Grid size={6}>
                       <input
+                        name="firstName"
                         type="text"
-                        className={styles.searchinput}
+                        className={styles.input}
                         placeholder="First Name*"
+                        onChange={signupFormik.handleChange}
+                        value={signupFormik.values.firstName}
+                        style={{
+                          border:
+                            signupFormik.touched.firstName &&
+                            signupFormik.errors.firstName
+                              ? "solid red 2px"
+                              : "",
+                        }}
                       />
+                      {/* show error: */}
+                      {signupFormik.touched.firstName &&
+                        signupFormik.errors.firstName && (
+                          <p
+                            className={styles.errorText}
+                            style={{ left: "2.8rem" }}
+                          >
+                            {signupFormik.errors.firstName}
+                          </p>
+                        )}
                     </Grid>
-                    <Grid size={6}>
+                    <Grid style={{ position: "relative" }} size={6}>
                       <input
+                        name="lastName"
                         type="text"
-                        className={styles.searchinput}
+                        className={styles.input}
                         placeholder="Last Name*"
+                        onChange={signupFormik.handleChange}
+                        value={signupFormik.values.lastName}
+                        style={{
+                          border:
+                            signupFormik.touched.lastName &&
+                            signupFormik.errors.lastName
+                              ? "solid red 2px"
+                              : "",
+                        }}
                       />
+                      {/* show error: */}
+                      {signupFormik.touched.lastName &&
+                        signupFormik.errors.lastName && (
+                          <p className={styles.errorText}>
+                            {signupFormik.errors.lastName}
+                          </p>
+                        )}
                     </Grid>
                     <Grid size={12}>
                       <input
+                        name="username"
                         type="text"
-                        className={styles.searchinput}
+                        className={styles.input}
                         placeholder="UserName*"
+                        onChange={signupFormik.handleChange}
+                        value={signupFormik.values.username}
+                        style={{
+                          border:
+                            signupFormik.touched.username &&
+                            signupFormik.errors.username
+                              ? "solid red 2px"
+                              : "",
+                        }}
                       />
+                      {/* show error: */}
+                      {signupFormik.touched.username &&
+                        signupFormik.errors.username && (
+                          <p
+                            className={styles.errorText}
+                            style={{ left: "2.8rem" }}
+                          >
+                            {signupFormik.errors.username}
+                          </p>
+                        )}
                     </Grid>
                     <Grid size={12}>
                       <input
+                        name="email"
                         type="email"
-                        className={styles.searchinput}
+                        className={styles.input}
                         placeholder="Email*"
+                        onChange={signupFormik.handleChange}
+                        value={signupFormik.values.email}
+                        style={{
+                          border:
+                            signupFormik.touched.email &&
+                            signupFormik.errors.email
+                              ? "solid red 2px"
+                              : "",
+                        }}
                       />
+                      {/* show error: */}
+                      {signupFormik.touched.email &&
+                        signupFormik.errors.email && (
+                          <p
+                            className={styles.errorText}
+                            style={{ left: "2.8rem" }}
+                          >
+                            {signupFormik.errors.email}
+                          </p>
+                        )}
                     </Grid>
                     <Grid size={12}>
                       <input
+                        name="password"
                         type="password"
-                        className={styles.searchinput}
+                        className={styles.input}
                         placeholder="Password*"
+                        onChange={signupFormik.handleChange}
+                        value={signupFormik.values.password}
+                        style={{
+                          border:
+                            signupFormik.touched.password &&
+                            signupFormik.errors.password
+                              ? "solid red 2px"
+                              : "",
+                        }}
                       />
+                      {/* show error: */}
+                      {signupFormik.touched.password &&
+                        signupFormik.errors.password && (
+                          <p
+                            className={styles.errorText}
+                            style={{ left: "2.8rem" }}
+                          >
+                            {signupFormik.errors.password}
+                          </p>
+                        )}
                     </Grid>
                     <Grid size={12}>
                       <input
+                        name="confirmPassword"
                         type="password"
-                        className={styles.searchinput}
-                        placeholder="Re Password*"
+                        className={styles.input}
+                        placeholder="Confirm Password*"
+                        onChange={signupFormik.handleChange}
+                        value={signupFormik.values.confirmPassword}
+                        style={{
+                          border:
+                            signupFormik.touched.confirmPassword &&
+                            signupFormik.errors.confirmPassword
+                              ? "solid red 2px"
+                              : "",
+                        }}
                       />
+                      {/* show error: */}
+                      {signupFormik.touched.confirmPassword &&
+                        signupFormik.errors.confirmPassword && (
+                          <p
+                            className={styles.errorText}
+                            style={{ left: "2.8rem" }}
+                          >
+                            {signupFormik.errors.confirmPassword}
+                          </p>
+                        )}
                     </Grid>
                     <Grid size={12}>
                       <p className={styles.tologin}>
                         You have already registered?{" "}
-                        <span className={styles.loginbtn}>login</span>
+                        <span
+                          onClick={() => swiperRef.current?.slidePrev()}
+                          className={styles.loginbtn}
+                        >
+                          login
+                        </span>
                       </p>
                     </Grid>
                   </Grid>
